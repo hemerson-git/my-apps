6c071cd8a80ff0cba72461187e1f0763e3df097d
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-08-17 23:42:20 -0300

    Add Slider

--- clock/src/pages/Pomodoro/index.tsx
+++ clock/src/pages/Pomodoro/index.tsx
@@ -1,6 +1,18 @@
-import { VStack } from "native-base";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Pressable,
+  Slider,
+  Text,
+  useTheme,
+  VStack,
+} from "native-base";
 
 import { CountdownProvider } from "../../contexts/CountdownContext";
+import Icon from "@expo/vector-icons/Feather";
 
 // COMPONENTS
 import { Timer } from "../../components/Timer";
@@ -8,8 +20,12 @@ import { Header } from "../../components/Header";
 import WorkingAnimation from "../../assets/working.json";
 
 export function Pomodoro() {
-  const TIME = 25 * 60; // 25 minutes
-  const INTERVAL = 5 * 60; // 5 minutes
+  const { colors } = useTheme();
+  const [maxTime, setMaxTime] = useState(25);
+  const [intervalTime, setIntervalMaxTime] = useState(5);
+
+  let TIME = maxTime * 60; // 25 minutes
+  let INTERVAL = intervalTime * 60; // 5 minutes
 
   return (
     <CountdownProvider timeInSeconds={TIME}>
@@ -17,14 +33,82 @@ export function Pomodoro() {
 
       <VStack
         flex={1}
-        justifyContent="center"
         alignContent="center"
         bg="gray.900"
         px="4"
         borderTopRadius={48}
         mt="32"
       >
-        <Timer initialTime={TIME} intervalTime={INTERVAL} />
+        <VStack alignItems="flex-end" py={"8"} px="2">
+          <IconButton
+            justifyContent="flex-end"
+            width={"12"}
+            mb="4"
+            icon={
+              <Icon name="settings" size={24} color={colors.primary[500]} />
+            }
+          />
+
+          <VStack px="4" space="4" mb="8">
+            <Heading color="white" fontSize="md">
+              Working Time:
+            </Heading>
+
+            <HStack w="full" space="4">
+              <Slider
+                size="md"
+                defaultValue={25}
+                maxValue={59}
+                minValue={15}
+                color="primary.500"
+                onChange={(value) => setMaxTime(value)}
+                flex={1}
+              >
+                <Slider.Track bg="primary.500">
+                  <Slider.FilledTrack bg="primary.500" />
+                </Slider.Track>
+
+                <Slider.Thumb bg="primary.500" />
+              </Slider>
+
+              <Text color="white" fontSize="md">
+                {maxTime}
+              </Text>
+            </HStack>
+          </VStack>
+
+          <VStack px="4" space="4">
+            <Heading color="white" fontSize="md">
+              Working Time:
+            </Heading>
+
+            <HStack w="full" space="4">
+              <Slider
+                size="md"
+                defaultValue={25}
+                maxValue={15}
+                minValue={0}
+                color="primary.500"
+                onChange={(value) => setMaxTime(value)}
+                flex={1}
+              >
+                <Slider.Track bg="primary.500">
+                  <Slider.FilledTrack bg="primary.500" />
+                </Slider.Track>
+
+                <Slider.Thumb bg="primary.500" />
+              </Slider>
+
+              <Text color="white" fontSize="md">
+                {maxTime}
+              </Text>
+            </HStack>
+          </VStack>
+        </VStack>
+
+        <VStack flex={1} justifyContent="center" px="4">
+          <Timer initialTime={TIME} intervalTime={INTERVAL} />
+        </VStack>
       </VStack>
     </CountdownProvider>
   );
