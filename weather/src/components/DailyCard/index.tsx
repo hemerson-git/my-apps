96b5cc31a3a25ed79f1510332b98d52385f1169c
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-09-04 22:01:23 -0300

    Finished Base Home Layout

--- weather/src/components/DailyCard/index.tsx
+++ weather/src/components/DailyCard/index.tsx
@@ -3,14 +3,21 @@ import { Feather } from "@expo/vector-icons";
 
 export function DailyCard() {
   return (
-    <VStack bg="primary.500" alignItems="center" px={4} borderRadius={"xl"}>
+    <VStack
+      bg="primary.500"
+      alignItems="center"
+      px={4}
+      py={2}
+      borderRadius={"xl"}
+      mr={2}
+    >
       <Text fontSize={48} color="primary.100" mb={3}>
         24º
       </Text>
 
       <Feather name="sun" size={48} />
 
-      <Text mt={3} color="primary.100" fontSize={16}>
+      <Text mt={3} color="primary.100" fontSize={18}>
         Amanhã
       </Text>
     </VStack>
--- weather/src/pages/Home/index.tsx
+++ weather/src/pages/Home/index.tsx
@@ -1,26 +1,32 @@
-import { FlatList, VStack } from "native-base";
+import { FlatList, Row, VStack } from "native-base";
 import { DailyCard } from "../../components/DailyCard";
+import { DayWidget } from "../../components/DayWidget";
 import { Header } from "../../components/Header";
 import { WeatherInfo } from "../../components/WeatherInfo";
 
 const list = [
   {
+    id: 1,
     temp: 48,
     day: "wendsday",
   },
   {
+    id: 2,
     temp: 48,
     day: "wendsday",
   },
   {
+    id: 3,
     temp: 48,
     day: "wendsday",
   },
   {
+    id: 4,
     temp: 48,
     day: "wendsday",
   },
   {
+    id: 5,
     temp: 48,
     day: "wendsday",
   },
@@ -28,16 +34,22 @@ const list = [
 
 export function Home() {
   return (
-    <VStack bg="primary.700" flex={1} pt={15} px={2}>
+    <VStack bg="primary.700" flex={1} py={15} px={2}>
       <Header />
       <WeatherInfo />
 
-      <FlatList
-        data={list}
-        renderItem={() => <DailyCard />}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-      />
+      <DayWidget />
+
+      <Row>
+        <FlatList
+          data={list}
+          renderItem={() => <DailyCard />}
+          keyExtractor={(item) => String(item.id)}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          fadingEdgeLength={100}
+        />
+      </Row>
     </VStack>
   );
 }
